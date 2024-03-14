e4c1348b0281ae285fd80a29d9f524813e8eed6c
Pond <6410685033@student.tu.ac.th>
2024-03-14 23:51:00 +0700

    complete navigation

--- API/Views/NewsView.swift
+++ API/Views/NewsView.swift
@@ -9,75 +9,82 @@ import SwiftUI
 
 struct NewsView: View {
     @State var news: ResponseBody?
-    @State var newsManager = NewsManager()
+    @State private var newsManager = NewsManager()
     @State private var keyword: String = ""
+    @State private var showingForm = true
     
     var body: some View {
-        if let news = news {
-            List(news.articles, id: \.url) { article in
-                VStack(alignment: .leading) {
-                    if let urlToImage = article.urlToImage, let imageUrl = URL(string: urlToImage) {
-                        AsyncImage(url: imageUrl) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                            case .success(let image):
-                                image.resizable().aspectRatio(contentMode: .fit)
-                            case .failure:
-                                Image(systemName: "photo")
-                            @unknown default:
-                                EmptyView()
-                            }
+        NavigationView {
+            if showingForm {
+                VStack {
+                    Text("News Reporter").font(.title)
+                    
+                    Form {
+                        Section(header: Text("News Query")) {
+                            TextField("Keyword", text: $keyword)
                         }
-                        .frame(height: 200)
-                    }
-                    
-                    Text(article.title)
-                        .font(.headline)
-                        .padding(.bottom, 2)
-                    
-                    if let author = article.author, !author.isEmpty {
-                        Text("By \(author)")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .padding(.bottom, 2)
-                    }
-                    
-                    if let description = article.description, !description.isEmpty {
-                        Text(description)
-                            .font(.body)
-                            .padding(.bottom, 2)
-                    }
-                }
-            }
-        } 
-        else {
-            VStack {
-                Text("News Reporter").font(.title)
-                
-                Form {
-                    Section(header: Text("News Query")) {
-                        TextField("Keyword", text: $keyword)
-    //                    TextField("Country", text: $country)
-    //                    TextField("Category", text: $category)
-                    }
-                    Button("Get News") {
-                        Task {
-                            do {
-                                try await news = newsManager.getCurrentNews(keyword: keyword)
-                            } catch {
-                                print("Error getting news \(error)")
+                        Button("Get News") {
+                            Task {
+                                do {
+                                    news = try await newsManager.getCurrentNews(keyword: keyword)
+                                    showingForm = false
+                                } catch {
+                                    print("Error getting news \(error)")
+                                }
                             }
                         }
                     }
                 }
+            } else {
+                List(news?.articles ?? [], id: \.url) { article in
+                    VStack(alignment: .leading) {
+                        if let urlToImage = article.urlToImage, 
+                            let imageUrl = URL(string: urlToImage) {
+                                AsyncImage(url: imageUrl) { phase in
+                                    switch phase {
+                                        case .empty:
+                                            ProgressView()
+                                        case .success(let image):
+                                            image.resizable().aspectRatio(contentMode: .fit)
+                                        case .failure:
+                                            Image(systemName: "photo")
+                                        @unknown default:
+                                            EmptyView()
+                                    }
+                                }
+                                .frame(height: 200)
+                            }
+                            Text(article.title)
+                            .font(.headline)
+                            .padding(.bottom, 2)
+                        
+                            if let author = article.author, !author.isEmpty {
+                                Text("By \(author)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .padding(.bottom, 2)
+                            }
+                            if let description = article.description, !description.isEmpty {
+                                Text(description)
+                                    .font(.body)
+                                    .padding(.bottom, 2)
+                            }
+                    }
+                }
+                .navigationBarTitle("News", displayMode: .inline)
+                .navigationBarItems(leading: Button(action: {
+                    showingForm = true
+                }) {
+                    Text("Back")
+                })
             }
         }
     }
 }
 
+
+
     
 #Preview {
-//    NewsView(news: previewNews)
     NewsView()
 }
